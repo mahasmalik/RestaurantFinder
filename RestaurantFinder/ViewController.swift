f3ff65b67a233fb24029b0421183f138915b9a99
Maha Malik <malikms@uci.edu>
2020-05-25 22:08:23 -0700

    Initial Commit

---
+++ RestaurantFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantFinder
+//
+//  Created by Maha Malik on 5/25/20.
+//  Copyright © 2020 Maha Malik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
